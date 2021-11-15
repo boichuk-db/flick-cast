e8de5c8e34e9b8d638e23a4ee7686a0a33fe43d3
Denys <boichuk.bd@gmail.com>
2021-11-15 12:07:44 +0200

    main-page

---
+++ components/index.js
@@ -0,0 +1,5 @@
+import Profiles from "./Profiles";
+import TabIcon from "./TabIcon";
+import ProgressBar from "./ProgressBar";
+
+export { TabIcon, Profiles, ProgressBar };
---
+++ navigation/tabs.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Home } from "../screens";
+import { COLORS, icons } from "../constants";
+
+import { TabIcon } from "../components";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.black,
+          borderTopColor: "transparent",
+        },
+        style: {
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          height: 100,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Play"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.play_button} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.profile} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
